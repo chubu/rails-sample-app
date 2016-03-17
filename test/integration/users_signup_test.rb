a6ff4b781e05f63a079a1e9f7d204914903a9af2
chubu <kentaro.enomoto@syoya.com>
2016-03-17 21:14:22 +0900

    Add error message and flash test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
   test "valid signup information" do
@@ -21,7 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         password_confirmation: "password" }
     end
     assert_template 'users/show'
-    assert_select 'div', 'Welcome to the Sample App!'
+    assert_not flash[:success].empty?
   end
 end
 
